b6a4947d91a1ca0c2c8887d07b8cb2183707c865
Derstilon <quban123@gmail.com>
2020-04-22 22:29:14 +0200

    Podstawowe generic templates

--- hallOfFameClient/urls.py
+++ hallOfFameClient/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
 from .admin import lecturer_admin_site
+from .views import MyView
+from .views import SubjectListView
 
 app_name = 'hallOfFameClient'
 urlpatterns = [
+    path('mine/', MyView.as_view(), name='my-view'),
+    path('subjects/', SubjectListView.as_view(), name='subject-list'),
     path('lecturer/', lecturer_admin_site.urls),
 ]
