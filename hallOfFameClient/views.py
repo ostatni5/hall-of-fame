091934116fd375b6a59d3504b0c24ad1b7a5a835
Derstilon <quban123@gmail.com>
2020-04-25 21:31:32 +0200

    beauty in the subjectList

--- hallOfFameClient/views.py
+++ hallOfFameClient/views.py
@@ -18,4 +18,7 @@ class SubjectListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['username'] = "Michał Krawczyk"
+        context['diagramUrl'] = "https://media.discordapp.net/attachments/689977881535053839/701202475906236456/unknown.png"
+        context['myAverage'] = "88"
+        context['semesterAverage'] = "76"
         return context
