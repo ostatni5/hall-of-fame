60f8907669d0b0e5fbba228aafa390950a970512
Derstilon <quban123@gmail.com>
2020-05-07 16:28:37 +0200

    Podpięte zaczątki widoków do templatesów

--- hallOfFameClient/urls.py
+++ hallOfFameClient/urls.py
@@ -5,14 +5,14 @@ from . import views
 from .admin import lecturer_admin_site
 from .views import TabView, DashboardLecturerView,StatView
 
-app_name = 'hallOfFameClient'
+app_name = 'lecturer'
 urlpatterns = [
+    path('dashboard', DashboardLecturerView.as_view(), name='dashboard'),
     # path('lecturer/hallOfFameClient/subject/<int:pk>')
 ]
 
 # OLD PATHS
 #     path('tab/', TabView.as_view(), name='tab-view'),
 #     path('dashboard', DashboardStudentView.as_view(), name='dashboard-student'),
-#     path('lecturer', DashboardLecturerView.as_view(), name='dashboard-lecturer'),
 #     path('subject', GroupStudentView.as_view(), name='group-student'),
 #     path('stat', StatView.as_view(), name='stat-view'),
