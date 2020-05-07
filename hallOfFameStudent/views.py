e7667fa5d7c20727ec0ad8f483945aa9b84e3e7f
Derstilon <quban123@gmail.com>
2020-05-07 17:12:09 +0200

    Komentarz z potrzebnymi zapytaniami

--- hallOfFameStudent/views.py
+++ hallOfFameStudent/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from hallOfFameClient.models import Student, Subject, StudentScore
+from polls.views import DetailView
+
+
+class RankingStudentView(DetailView):
+    def get_context_data(self, **kwargs):
+        return "DETALE BITCH!"
+    """
+    Potrzeba:   <-- do tego trzeba ogarnąć Chart.js albo coś podobnego, więc to można później
+        User,
+        Wykres słubkowy
+                Lista grup Usera,
+                Średnia w danej grupie
+                Średnia z przedmiotu ogólna
+        Lista rankingu osób w Roczniku
+                User
+                Średnia całkowita
+                Miejsce w rankingu (egzekwo mają to samo miejsce)
+        Dane do wykresu RANKING W CZASIE 
+                kolejność w rankingu w danym miesiącu
+                średnia pw danym miesiącu
+    """
 
 class GroupStudentView(ListView):
     # sub_id = sub_id
@@ -19,13 +40,29 @@ class GroupStudentView(ListView):
         context['myRanking'] = "15"
         context['groupStudents'] = self.groupStudents
         return context
+    """
+    Potrzeba:
+        User,
+        Grupa o id 'sub_id', <-- nazwę zmienię potem, bo teraz to jest id przedmiotu, ale zmieni się to 
+        Twoja średnia w grupie (z przedmiotu),
+        Twój ranking (egzekwo mają to samo miejsce)
+        Lista ocen z zadań na zajęciach
+                wszystkie dane z zadania
+                ocena
+                procentowo
+        Lista rankingu osób w grupie
+                User
+                Średnia
+                Miejsce w rankingu (egzekwo mają to samo miejsce)
+        Dane do wykresu RANKING W CZASIE <-- do tego trzeba ogarnąć Chart.js albo coś podobnego, więc to można później
+                kolejność w rankingu po danym zadaniu
+                średnia po danym zadaniu
+    """
 
 class DashboardStudentView(ListView):
     template_name = 'hallOfFameClient/dashboard_student.html'
     student = Student.objects.filter(album_number=213700).first()
     model = Subject
-
-    # paginate_by = 100  # if pagination is desired
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -35,3 +72,15 @@ class DashboardStudentView(ListView):
         context['myAverage'] = "88"
         context['semesterAverage'] = "76"
         return context
+    """
+    Potrzeba:
+        User,
+        Średnia Usera (całkowita),
+        Średnia wszystkich na roku (całkowita),
+        Dane do wykresu <- to się później ustali
+        Lista grup w których ma zajęcia
+                Nazwa przedmiotu grupy
+                Nazwa grupy
+                opis krótki
+                ECTS
+    """
