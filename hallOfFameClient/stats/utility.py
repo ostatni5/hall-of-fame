780001d327d8196d19ba031e8202dfe5cb92c2d0
Szymon <ostatni5@o2.pl>
2020-05-10 22:08:27 +0200

    change requests to bulk

--- hallOfFameClient/stats/utility.py
+++ hallOfFameClient/stats/utility.py
@@ -1,7 +1,11 @@
+import json
+from datetime import datetime
+
+from django.core import serializers
 from django.db.models import Sum, Q, F, Avg
 
 from hallOfFameClient.models import Subject, Student, Group, StudentScore, StatGroupScore, StatGroupStudentScore, \
-    StatSubjectScore, StatSubjectStudentScore
+    StatSubjectScore, StatSubjectStudentScore, ArchiveRecord, Variable
 
 Student.objects.values('pk', 'groups__pk', 'groups__stat_score__max_score').annotate(score=Sum('scores__value'), )
 
@@ -19,17 +23,18 @@ def calcStatsSubject():
     StatSubjectStudentScore.objects.all().delete()
 
     query_group = Group.objects.values('pk', ).annotate(max_score=Sum('exercises__max_score'))
-
+    objs = []
     for res in query_group:
         obj = StatGroupScore()
         obj.group_id = res['pk']
         obj.max_score = res['max_score'] or 0
         obj.mean_value = 0
-        obj.save()
+        objs.append(obj)
+    StatGroupScore.objects.bulk_create(objs)
 
     query_students = Student.objects.values('pk', 'groups__pk', 'groups__stat_score__pk',
                                             'groups__stat_score__max_score').annotate(score=Sum('scores__value'), )
-
+    objs = []
     for res in query_students:
         obj = StatGroupStudentScore()
         obj.student_id = res['pk']
@@ -37,34 +42,46 @@ def calcStatsSubject():
         obj.value = res['score']
         val = res['groups__stat_score__max_score']
         obj.mean_value = res['score'] * 100 / val if val != 0 else 0
-        obj.save()
+        objs.append(obj)
+    StatGroupStudentScore.objects.bulk_create(objs)
 
     query_group_avg = StatGroupStudentScore.objects.values('stat_group__pk').annotate(mean_value=Avg('mean_value'), )
 
+    objs = []
     for res in query_group_avg:
         obj = StatGroupScore.objects.get(pk=res['stat_group__pk'])
         obj.mean_value = res['mean_value']
-        obj.save()
+        objs.append(obj)
+    StatGroupScore.objects.bulk_update(objs, ['mean_value'])
 
     query_subject_avg = StatGroupScore.objects.values('group__subject__pk').annotate(mean_value=Avg('mean_value'), )
-
+    objs = []
     for res in query_subject_avg:
         obj = StatSubjectScore()
         obj.subject_id = res['group__subject__pk']
         obj.mean_value = res['mean_value']
-        obj.save()
+        objs.append(obj)
+    StatSubjectScore.objects.bulk_create(objs)
 
     query_subject_student = StatGroupStudentScore \
         .objects.values('student__pk',
                         'stat_group__group__subject__pk') \
         .annotate(mean_value=Avg('mean_value'), )
-
+    objs = []
     for res in query_subject_student:
         obj = StatSubjectStudentScore()
         obj.subject_id = res['stat_group__group__subject__pk']
         obj.student_id = res['student__pk']
         obj.mean_value = res['mean_value']
-        obj.save()
+        objs.append(obj)
+    StatSubjectStudentScore.objects.bulk_create(objs)
+
+    arch_record = ArchiveRecord()
+    arch_record.save()
+    jsonDate = arch_record.creation_date.strftime('%Y-%m-%d %H:%M')
+    # last_stats = Variable.objects.get_or_create(key="last_stats", defaults={"value": jsonDate})
+    last_date = datetime.strptime(jsonDate, '%Y-%m-%d %H:%M')
+    print(last_date)
 
 
 def rankingSubject(subject_pk):
